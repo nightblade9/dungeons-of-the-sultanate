2cb78ce21828e0baa282bd251dd7e2fdfefd5478
nightblade9 <nightbladecodes@gmail.com>
2022-08-29 17:13:50 -0400

    Hello World sample with bean-printing code

---
+++ src/main/java/com/deengames/dungeonsofthesultantate/controllers/HomeController.java
@@ -0,0 +1,14 @@
+package com.deengames.dungeonsofthesultantate.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController
+{
+    @GetMapping("/")
+    public String index()
+    {
+        return "Hello, Dungeons of the Sultanate!";
+    }
+}
