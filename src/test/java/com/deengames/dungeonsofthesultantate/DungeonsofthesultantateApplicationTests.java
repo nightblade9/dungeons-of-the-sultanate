71a428f5cce56cce3f3cc3baec2e2964cb23b9b3
nightblade9 <nightbladecodes@gmail.com>
2022-08-29 16:53:47 -0400

    Temporarily comment out DB stuff so app/tests run

--- src/test/java/com/deengames/dungeonsofthesultantate/DungeonsofthesultantateApplicationTests.java
+++ src/test/java/com/deengames/dungeonsofthesultantate/DungeonsofthesultantateApplicationTests.java
@@ -2,12 +2,18 @@ package com.deengames.dungeonsofthesultantate;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
 
 @SpringBootTest
 class DungeonsofthesultantateApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void testCodeWorks() {
+		Assert.isTrue(2 + 3 == 5);
 	}
 
 }
