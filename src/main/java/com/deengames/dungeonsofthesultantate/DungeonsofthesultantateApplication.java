c29e44e082d4e2a0fbc3d3306871766613523ae9
nightblade9 <nightbladecodes@gmail.com>
2022-08-29 16:40:27 -0400

    Spring Initializr Spring Boot 2.73, Spring Web, OAuth2 Client, JDBC API, Spring Data MongoDB, Quartz

---
+++ src/main/java/com/deengames/dungeonsofthesultantate/DungeonsofthesultantateApplication.java
@@ -0,0 +1,13 @@
+package com.deengames.dungeonsofthesultantate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DungeonsofthesultantateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DungeonsofthesultantateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deengames/dungeonsofthesultantate/DungeonsofthesultantateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deengames.dungeonsofthesultantate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DungeonsofthesultantateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
