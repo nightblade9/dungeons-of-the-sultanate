cf5bafec9bcbe02deac58804056d2f45b8408cd0
nightblade9 <nightbladecodes@gmail.com>
2022-08-31 14:34:15 -0400

    Get lombok to work with unit tests, via plugin (not dependency)

--- src/test/java/com/deengames/dungeonsofthesultantate/DungeonsofthesultantateApplicationTests.java
+++ src/test/java/com/deengames/dungeonsofthesultantate/DungeonsofthesultantateApplicationTests.java
@@ -1,5 +1,6 @@
 package com.deengames.dungeonsofthesultantate;
 
+import com.deengames.dungeonsofthesultantate.models.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.util.Assert;
@@ -16,4 +17,16 @@ class DungeonsOfTheSultantateApplicationTests {
 		Assert.isTrue(2 + 3 == 5);
 	}
 
+	@Test
+	void lombokMagicWorks()
+	{
+		// For lombok to work in tests, specify it as a plugin, not an oldschool Gradle dependency.
+		// Arrange
+		User user = new User();
+		var expectedId = "1031";
+		user.setId(expectedId);
+
+		// Act/Assert
+		Assert.isTrue(user.getId() == expectedId);
+	}
 }
