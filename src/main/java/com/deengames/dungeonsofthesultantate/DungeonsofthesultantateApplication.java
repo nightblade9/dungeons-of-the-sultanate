92ef595aeddfd5b44fe15ad40e86f79ef1f091dd
nightblade9 <nightbladecodes@gmail.com>
2022-08-31 11:46:47 -0400

    Refactor: rename class

--- src/main/java/com/deengames/dungeonsofthesultantate/DungeonsofthesultantateApplication.java
+++ src/main/java/com/deengames/dungeonsofthesultantate/DungeonsofthesultantateApplication.java
@@ -1,18 +1,13 @@
 package com.deengames.dungeonsofthesultantate;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
-
-import java.util.Arrays;
 
 @SpringBootApplication
-public class DungeonsofthesultantateApplication {
+public class DungeonsOfTheSultantateApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DungeonsofthesultantateApplication.class, args);
+		SpringApplication.run(DungeonsOfTheSultantateApplication.class, args);
 	}
 
 //	@Bean
--- src/test/java/com/deengames/dungeonsofthesultantate/DungeonsofthesultantateApplicationTests.java
+++ src/test/java/com/deengames/dungeonsofthesultantate/DungeonsofthesultantateApplicationTests.java
@@ -5,7 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.util.Assert;
 
 @SpringBootTest
-class DungeonsofthesultantateApplicationTests {
+class DungeonsOfTheSultantateApplicationTests {
 
 	@Test
 	void contextLoads() {
