bf37b8bc4b7da6b38f910f5db1eb6f7dd4fefa36
nightblade9 <nightbladecodes@gmail.com>
2022-09-19 22:49:01 -0400

    Re-fix OAuth2 integration for Google, 'twas broken.

--- src/main/java/com/deengames/dungeonsofthesultanate/users/UserController.java
+++ src/main/java/com/deengames/dungeonsofthesultanate/users/UserController.java
@@ -1,11 +1,17 @@
 package com.deengames.dungeonsofthesultanate.users;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.view.RedirectView;
+
+import java.security.Principal;
 
 //// Wires up the UI/view, via the UserService
 @RestController
@@ -19,14 +25,32 @@ public class UserController {
     @GetMapping("/user/onLogin")
     public RedirectView postLogin() throws UsernameNotFoundException {
         var authentication = SecurityContextHolder.getContext().getAuthentication();
-
+        var userLogin = getLogin(authentication);
         // add user into database (if not there already), and update lastLogin
-        var user = insertOrGetUser("herpderp");
+        var user = insertOrGetUser(userLogin);
 
         // update
 
         // Redirect. This doesn't trigger the controller, IDK why (I get a 500 error).
         return new RedirectView("/map/world");
+    }
+
+    // TODO: doesn't belong here
+    // TODO: refactor to use strategy pattern to encapsulate OAuth-provider specifics
+    private String getLogin(Authentication authentication) throws UsernameNotFoundException
+    {
+        if (authentication instanceof OAuth2AuthenticationToken)
+        {
+            var principal = (DefaultOAuth2User)(authentication.getPrincipal());
+            return principal.getAttribute("login").toString();
+        }
+        else if (authentication.getPrincipal() instanceof DefaultOAuth2User)
+        {
+            var principal = (DefaultOidcUser)(authentication.getPrincipal());
+            return principal.getAttribute("name");
+        }
+
+        throw new UsernameNotFoundException("OAuth2 provider not supported");
     }
 
     private UserModel insertOrGetUser(String username) throws UsernameNotFoundException {
