afa0a2e8413e1755fed19e68cf7f7b64cac56dc0
nightblade9 <nightbladecodes@gmail.com>
2022-09-22 14:00:41 -0400

    Map locations are data-driven :ok_hand:

--- src/main/java/com/deengames/dungeonsofthesultanate/maps/WorldMapController.java
+++ src/main/java/com/deengames/dungeonsofthesultanate/maps/WorldMapController.java
@@ -1,4 +1,4 @@
-package com.deengames.dungeonsofthesultanate.worldmap;
+package com.deengames.dungeonsofthesultanate.maps;
 
 import com.deengames.dungeonsofthesultanate.security.TokenParser;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -10,11 +10,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class WorldMapController
 {
     @GetMapping("/map/world")
-    public String worldMap(Model model) throws Exception
-    {
+    public String worldMap(Model model) {
+        // Used because this is where you reach on authentication
         var authentication = SecurityContextHolder.getContext().getAuthentication();
         var userEmailAddress = TokenParser.getUserEmailAddressFromToken(authentication);
         model.addAttribute("authenticatedAs", userEmailAddress);
+
+        model.addAttribute("locations", WorldMapLocations.LOCATIONS);
         return "map/world/index";
     }
 }
---
+++ src/main/java/com/deengames/dungeonsofthesultanate/maps/WorldMapLocations.java
@@ -0,0 +1,5 @@
+package com.deengames.dungeonsofthesultanate.maps;
+
+public class WorldMapLocations {
+    public static final String[] LOCATIONS = { "Towering Tree Forest" };
+}
