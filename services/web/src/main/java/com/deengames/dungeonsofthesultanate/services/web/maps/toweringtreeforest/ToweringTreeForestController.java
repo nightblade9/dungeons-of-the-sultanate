0115e575f8f3b56fb7aa3d161a95208764b55e88
nightblade9 <nightbladecodes@gmail.com>
2022-09-23 12:38:21 -0400

    PoC: web puts a message in RabbitMQ

--- services/web/src/main/java/com/deengames/dungeonsofthesultanate/services/web/maps/toweringtreeforest/ToweringTreeForestController.java
+++ services/web/src/main/java/com/deengames/dungeonsofthesultanate/services/web/maps/toweringtreeforest/ToweringTreeForestController.java
@@ -1,16 +1,55 @@
 package com.deengames.dungeonsofthesultanate.services.web.maps.toweringtreeforest;
 
 import com.deengames.dungeonsofthesultanate.services.web.maps.world.WorldMapLocations;
+import org.springframework.amqp.core.AmqpAdmin;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.Queue;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Date;
+
 @Controller
 public class ToweringTreeForestController
 {
+    @Autowired
+    private AutowireCapableBeanFactory autowireCapableBeanFactory;
+
     @GetMapping("/map/towering-tree-forest")
-    public String worldMap(Model model) {
+    public String worldMap(Model model) throws InstantiationException, IllegalAccessException {
         model.addAttribute("location", WorldMapLocations.locations[0]);
+
+        var thing = MessagingProofOfConcept.class.newInstance();
+        autowireCapableBeanFactory.autowireBean(thing);
+        thing.putMessage();
         return "map/toweringtreeforest/index";
+    }
+
+    static class MessagingProofOfConcept
+    {
+        @Autowired
+        private AmqpAdmin amqpAdmin;
+
+        @Autowired
+        private AmqpTemplate amqpTemplate;
+
+        private final String queueName = "testQueue";
+
+        @Bean
+        public Queue myQueue() {
+            return new Queue(queueName, false);
+        }
+
+        public void putMessage()
+        {
+            String text = String.format("hello at %s!", new Date());
+            amqpTemplate.convertAndSend(queueName, text);
+        }
     }
 }
