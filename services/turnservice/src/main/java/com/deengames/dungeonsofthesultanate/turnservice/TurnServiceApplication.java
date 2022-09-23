b34e21bc59f99f58ac7c5058a5010980fddde800
nightblade9 <nightbladecodes@gmail.com>
2022-09-23 16:14:42 -0400

    New JEE spring boot web service for turn service

---
+++ services/turnservice/src/main/java/com/deengames/dungeonsofthesultanate/turnservice/TurnServiceApplication.java
@@ -0,0 +1,13 @@
+package com.deengames.dungeonsofthesultanate.turnservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurnServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurnServiceApplication.class, args);
+	}
+
+}
