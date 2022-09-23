9db84395e79b14b47a487d0218c614a46254a00f
nightblade9 <nightbladecodes@gmail.com>
2022-09-23 11:20:18 -0400

    Fix case: turn service

--- services/turnservice/src/test/java/com/deengames/dungeonsofthesultanate/turnservice/TurnServiceApplicationTests.java
+++ services/turnservice/src/test/java/com/deengames/dungeonsofthesultanate/turnservice/TurnServiceApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TurnserviceApplicationTests {
+class TurnServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
