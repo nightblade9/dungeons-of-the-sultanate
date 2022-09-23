05ffbe5d1a2a2ffd68d7c980aa45aaaf6c960e28
nightblade9 <nightbladecodes@gmail.com>
2022-09-23 11:18:19 -0400

    Generated spring boot JAR-producing turn service stub with JPA and stuff

---
+++ services/turnservice/src/main/java/com/deengames/dungeonsofthesultanate/turnservice/TurnserviceApplication.java
@@ -0,0 +1,13 @@
+package com.deengames.dungeonsofthesultanate.turnservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurnserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurnserviceApplication.class, args);
+	}
+
+}
---
+++ services/turnservice/src/test/java/com/deengames/dungeonsofthesultanate/turnservice/TurnserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deengames.dungeonsofthesultanate.turnservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurnserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
