c67e825eef0f8b588d6c6b5131ed21058e27b7f5
nightblade9 <nightbladecodes@gmail.com>
2022-09-27 14:54:51 -0400

    Temporarily add "hello world" service to turn service, for testing.

--- services/turnservice/src/main/java/com/deengames/dungeonsofthesultanate/turnservice/core/PlayerController.java
+++ services/turnservice/src/main/java/com/deengames/dungeonsofthesultanate/turnservice/core/PlayerController.java
@@ -2,6 +2,7 @@ package com.deengames.dungeonsofthesultanate.turnservice.core;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,13 @@ public class PlayerController {
 
     @Autowired
     private TurnService service;
+
+    // TODO: delete
+    @GetMapping(value="/player")
+    public String helloWorld()
+    {
+        return "Hello, world!";
+    }
 
     @PostMapping(value = "/player", consumes = MediaType.TEXT_PLAIN_VALUE)
     public void createPlayer(@RequestBody String userId)
