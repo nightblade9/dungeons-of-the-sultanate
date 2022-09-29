705c79475807af862b5bf98bd779f5450014ccc8
nightblade9 <nightbladecodes@gmail.com>
2022-09-28 21:45:33 -0400

    Fix integration tests: stub out calls to turn service.

--- services/web/src/test/java/com/deengames/dungeonsofthesultanate/users/UserControllerIntegrationTest.java
+++ services/web/src/test/java/com/deengames/dungeonsofthesultanate/users/UserControllerIntegrationTest.java
@@ -3,13 +3,18 @@ package com.deengames.dungeonsofthesultanate.users;
 import com.deengames.dungeonsofthesultanate.BaseIntegrationTest;
 import com.deengames.dungeonsofthesultanate.services.web.security.SecurityContextFetcher;
 import com.deengames.dungeonsofthesultanate.security.StubToken;
+import com.deengames.dungeonsofthesultanate.services.web.security.client.ServiceToServiceClient;
 import com.deengames.dungeonsofthesultanate.services.web.users.UserController;
 import com.deengames.dungeonsofthesultanate.services.web.users.UserModel;
 import org.bson.types.ObjectId;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 import java.util.Date;
 
@@ -17,6 +22,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.BDDMockito.given;
 
+// Integration-ish; we mock service-to-service calls.
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class UserControllerIntegrationTest extends BaseIntegrationTest {
 
@@ -28,13 +34,23 @@ public class UserControllerIntegrationTest extends BaseIntegrationTest {
     @MockBean
     private SecurityContextFetcher securityContextFetcher;
 
+    // Mocks our service-to-service REST client so we can run without other services
+    @MockBean
+    private ServiceToServiceClient s2sClient;
+
+    @Autowired
+    private BeanFactory beanFactory;
+
     @Test
-    public void onlogin_InsertsUserIntoDatabase() {
+    public void onlogin_InsertsUserIntoDatabase() throws UsernameNotFoundException {
         // Arrange
         var expectedEmailAddress = "fake@fake.com";
 
         given(this.securityContextFetcher.getAuthentication())
                 .willReturn(new StubToken(expectedEmailAddress));
+
+        given(this.s2sClient.post(Mockito.anyString(), Mockito.any(), Mockito.any(), Mockito.any()))
+            .willReturn("hi there");
 
         // Act
         controller.onLogin();
@@ -51,7 +67,10 @@ public class UserControllerIntegrationTest extends BaseIntegrationTest {
         var expectedEmailAddress = "fake@fake.com";
 
         given(this.securityContextFetcher.getAuthentication())
-                .willReturn(new StubToken(expectedEmailAddress));
+            .willReturn(new StubToken(expectedEmailAddress));
+
+        given(this.s2sClient.post(Mockito.anyString(), Mockito.any(), Mockito.any(), Mockito.any()))
+            .willReturn("hi there");
 
         var expectedId = new ObjectId();
         var creationDate = new Date();
