170395bc55ad3769a20739c9cb859dd636140fe5
nightblade9 <nightbladecodes@gmail.com>
2022-10-01 15:41:02 -0400

    WIP

--- services/turnservice/src/main/java/com/deengames/dungeonsofthesultanate/turnservice/core/PlayerController.java
+++ services/turnservice/src/main/java/com/deengames/dungeonsofthesultanate/turnservice/core/PlayerController.java
@@ -2,10 +2,7 @@ package com.deengames.dungeonsofthesultanate.turnservice.core;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class PlayerController {
@@ -13,17 +10,20 @@ public class PlayerController {
     @Autowired
     private TurnService service;
 
-    // TODO: delete
     @GetMapping(value="/player", consumes=MediaType.APPLICATION_JSON_VALUE)
     public int getPlayer(String userId) {
         return service.getNumTurns(userId);
     }
 
     @PostMapping(value = "/player", consumes = MediaType.TEXT_PLAIN_VALUE)
-    public void createPlayer(@RequestBody String userId)
-    {
+    public void createPlayer(@RequestBody String userId) {
         var data = new PlayerTurns();
         data.setUserId(userId);
         service.addNewRecord(data);
+    }
+
+    @PatchMapping(value="/player", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void consumeTurn(@RequestBody String userId) {
+
     }
 }
