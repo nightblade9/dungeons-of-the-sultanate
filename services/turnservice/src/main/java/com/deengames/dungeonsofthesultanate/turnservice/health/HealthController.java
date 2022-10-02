90f0c7bac3d1879778f9f3a1f5ba408e3d19727f
nightblade9 <nightbladecodes@gmail.com>
2022-10-01 21:25:18 -0400

    Turn service health check is too smart, O(n) table scan for elapsed cron runtime

--- services/turnservice/src/main/java/com/deengames/dungeonsofthesultanate/turnservice/health/HealthController.java
+++ services/turnservice/src/main/java/com/deengames/dungeonsofthesultanate/turnservice/health/HealthController.java
@@ -1,5 +1,6 @@
 package com.deengames.dungeonsofthesultanate.turnservice.health;
 
+import com.deengames.dungeonsofthesultanate.turnservice.core.TurnRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.http.MediaType;
@@ -7,7 +8,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.Duration;
 import java.time.Instant;
+import java.time.OffsetDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -19,6 +22,9 @@ public class HealthController {
     // Client secret DOES have to be protected, so don't even store it in a variable
     @Autowired
     private Environment env;
+
+    @Autowired
+    private TurnRepository turnRepository;
 
     // This is the only health end-point that's unauthenticated, because it checks things that tell
     // us if authentication is working or broken.
@@ -36,6 +42,29 @@ public class HealthController {
         toReturn.put("s2s_auth_set", String.valueOf(isServiceToServiceSecretSet));
         toReturn.put("ticks_cron", tickCron);
 
+        Duration onTickElapsedTime = getOnTickElapsedTime();
+        toReturn.put("ticks_runtime", onTickElapsedTime.toMillis() + "ms");
         return toReturn;
+    }
+
+    private Duration getOnTickElapsedTime() {
+
+        // This is O(n) for n players (table scan - unindexed column), so use sparingly...
+        var allTicks = turnRepository.findAll();
+
+        var minTime = OffsetDateTime.MAX;
+        var maxTime = OffsetDateTime.MIN;
+        for (var t : allTicks) {
+            var tick = t.getLastTurnTickUtc();
+            if (tick.toEpochSecond() < minTime.toEpochSecond()) {
+                minTime = tick;
+            }
+            if (tick.toEpochSecond() > maxTime.toEpochSecond()) {
+                maxTime = tick;
+            }
+        }
+
+        var diff = Duration.between(minTime, maxTime);
+        return diff;
     }
 }
