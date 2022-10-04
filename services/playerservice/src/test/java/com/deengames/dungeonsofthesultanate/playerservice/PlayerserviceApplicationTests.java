118de43dc065b5c6638b70e5101f9d0f610a2c95
nightblade9 <nightbladecodes@gmail.com>
2022-10-04 16:07:37 -0400

    Hello World player service, with Mongo (idk if this is the right decision).
    Update all microservices to include spring-boot configuration processor.

---
+++ services/playerservice/src/test/java/com/deengames/dungeonsofthesultanate/playerservice/PlayerserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deengames.dungeonsofthesultanate.playerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayerserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
