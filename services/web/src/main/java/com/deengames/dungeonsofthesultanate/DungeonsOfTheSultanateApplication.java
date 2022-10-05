985dab1a0ead3df9d646758b50bf12428e7da868
nightblade9 <nightbladecodes@gmail.com>
2022-10-05 17:04:11 -0400

    Fix: default profile doesn't work with tests for web...? \o/

--- services/web/src/main/java/com/deengames/dungeonsofthesultanate/DungeonsOfTheSultanateApplication.java
+++ services/web/src/main/java/com/deengames/dungeonsofthesultanate/DungeonsOfTheSultanateApplication.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class DungeonsOfTheSultanateApplication extends WebSecurityConfigurerAdapter {
 
 	public static void main(String[] args) {
+		// Set the default profile to `dev`, see README.md for why (TL;DR OAuth2 client ID/secret)
+		System.setProperty(org.springframework.core.env.AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "dev");
 		SpringApplication.run(DungeonsOfTheSultanateApplication.class, args);
 	}
 }
