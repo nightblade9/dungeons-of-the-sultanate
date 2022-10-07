ea3f67cb5ad14072a379d03e9334470a37caa87a
nightblade9 <nightbladecodes@gmail.com>
2022-10-07 16:16:18 -0400

    Hello World encounter service - no DB though

---
+++ services/encounterservice/src/main/java/com/deengames/dungeonsofthesultanate/encounterservice/EncounterServiceApplication.java
@@ -0,0 +1,13 @@
+package com.deengames.dungeonsofthesultanate.encounterservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncounterServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncounterServiceApplication.class, args);
+	}
+
+}
---
+++ services/encounterservice/src/test/java/com/deengames/dungeonsofthesultanate/encounterservice/EncounterServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deengames.dungeonsofthesultanate.encounterservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncounterServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
