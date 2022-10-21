edb33fe82b92df4c802123e2fcf7f2013ec786e6
nightblade9 <nightbladecodes@gmail.com>
2022-10-21 16:28:46 -0400

    Random encounter drops PoC (model/back-end only)

--- services/encounterservice/src/main/java/com/deengames/dungeonsofthesultanate/encounterservice/encounters/EncounterPicker.java
+++ services/encounterservice/src/main/java/com/deengames/dungeonsofthesultanate/encounterservice/encounters/EncounterPicker.java
@@ -1,9 +1,52 @@
 package com.deengames.dungeonsofthesultanate.encounterservice.encounters;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Random;
+
 public class EncounterPicker {
 
+    static class EncounterTypeWithWeight
+    {
+        public EncounterType encounterType;
+        public float weight;
+
+        public EncounterTypeWithWeight(EncounterType type, float weight) {
+            this.encounterType = type;
+            this.weight = weight;
+        }
+    }
+
+    private static HashMap<Location, ArrayList<EncounterTypeWithWeight>> locationEncounters = new HashMap<>();
+
     public static EncounterType chooseEncounter(Location location) {
-        // TODO: pick a design that scales with locations and encounter types
-        return EncounterType.BATTLE;
+        // Initialize if not initialized correctly
+        if (!locationEncounters.containsKey(location)) {
+            locationEncounters.clear();
+
+            var forestEncounters = new ArrayList<EncounterTypeWithWeight>();
+            forestEncounters.add(new EncounterTypeWithWeight(EncounterType.BATTLE, 0.95f));
+            forestEncounters.add(new EncounterTypeWithWeight(EncounterType.CURRENCY_DROP, 0.05f));
+
+            locationEncounters.put(Location.TOWERING_TREE_FOREST, forestEncounters);
+        }
+
+        // Pick, now. Weighted pick, c/o https://stackoverflow.com/questions/673283/weighted-randomness-in-java
+        var totalWeight = 0f;
+        var ourTypes = locationEncounters.get(location);
+        for (var locationData : ourTypes) {
+            totalWeight += locationData.weight;
+        }
+
+        var index = 0;
+        for (var r = Math.random() * totalWeight; index < ourTypes.size() - 1; ++index) {
+            r -= ourTypes.get(index).weight;
+            if (r <= 0) {
+                break;
+            }
+        }
+        var toReturn = ourTypes.get(index);
+        return toReturn.encounterType;
     }
 }
---
+++ services/encounterservice/src/test/java/com/deengames/dungeonsofthesultanate/encounterservice/encounters/EncounterPickerTests.java
@@ -0,0 +1,29 @@
+package com.deengames.dungeonsofthesultanate.encounterservice.encounters;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+public class EncounterPickerTests {
+
+    @Test
+    public void chooseEncounter_RandomlyChoosesWeightedEncounters() {
+        var results = new ArrayList<EncounterType>();
+
+        // Act
+        while (results.size() < 100) {
+            var encounter = EncounterPicker.chooseEncounter(Location.TOWERING_TREE_FOREST);
+            results.add(encounter);
+        }
+
+        // Assert
+        var numBattles = results.stream().filter(e -> e == EncounterType.BATTLE).count();
+        var numCurrencyDrops = results.stream().filter(e -> e == EncounterType.CURRENCY_DROP).count();
+
+        // Conservative ranges make for stable tests 👌
+        Assertions.assertThat(numBattles >= 90);
+        Assertions.assertThat(numCurrencyDrops <= 10);
+    }
+
+}
