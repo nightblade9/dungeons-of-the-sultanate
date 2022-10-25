785246085507435f31f9328c9746feb6c6907cfd
nightblade9 <nightbladecodes@gmail.com>
2022-10-25 17:01:11 -0400

    Header that shows logout link when logged in

--- services/web/src/main/java/com/deengames/dungeonsofthesultanate/web/home/HomeController.java
+++ services/web/src/main/java/com/deengames/dungeonsofthesultanate/web/home/HomeController.java
@@ -1,19 +1,8 @@
 package com.deengames.dungeonsofthesultanate.web.home;
 
 import com.deengames.dungeonsofthesultanate.web.BaseController;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AnonymousAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import java.util.Collections;
-import java.util.Map;
 
 @Controller
 public class HomeController extends BaseController
